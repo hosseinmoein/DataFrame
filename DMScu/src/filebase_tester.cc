3808b7ebb7e4f314f40476a808343b697d8f1f1f
hosseinmoein <31854960+hosseinmoein@users.noreply.github.com>
2017-10-28 14:22:16 -0400

    Update filebase_tester.cc

--- DMScu/src/filebase_tester.cc
+++ DMScu/src/filebase_tester.cc
@@ -1,5 +1,9 @@
 // Hossein Moein
 // September 25, 2007
+// To the extent possible under law, the author(s) have dedicated all
+// copyright and related and neighboring rights to this software to
+// the public domain worldwide. This software is distributed without
+// any warranty.
 
 #include <DMScu_FileBase.h>
 

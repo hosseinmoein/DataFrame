07b50e87372bb0b3baf121f77928837f1a45edfb
hossein.moein@kensho.com <hossein.moein@kensho.com>
2018-10-31 17:47:07 -0400

    separated DataFrame_functors.h from DataFrame.h

--- src/CommonMakefile.mk
+++ src/CommonMakefile.mk
@@ -23,6 +23,7 @@ HEADERS = $(LOCAL_INCLUDE_DIR)/HeteroVector.h \
           $(LOCAL_INCLUDE_DIR)/DataFrame_get.tcc \
           $(LOCAL_INCLUDE_DIR)/DataFrame_read.tcc \
           $(LOCAL_INCLUDE_DIR)/DataFrame_opt.tcc \
+          $(LOCAL_INCLUDE_DIR)/DataFrame_functors.h \
           $(LOCAL_INCLUDE_DIR)/DFVisitors.h \
           $(LOCAL_INCLUDE_DIR)/VectorView.h
 

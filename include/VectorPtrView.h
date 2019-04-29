1a60c0d1f5074ee69d4946735750c886a078081f
hossein.moein@kensho.com <hossein.moein@kensho.com>
2019-04-29 17:41:44 -0400

    More work toward ptr vector

--- include/VectorPtrView.h
+++ include/VectorPtrView.h
@@ -1,5 +1,7 @@
 // Hossein Moein
-// July 17 2009
+// April 28, 2019
+// Copyright (C) 2019-2021 Hossein Moein
+// Distributed under the BSD Software License (see file License)
 
 #pragma once
 
@@ -8,6 +10,9 @@
 
 // ----------------------------------------------------------------------------
 
+namespace hmdf
+{
+	
 template<typename T>
 class   VectorPtrView  {
 
@@ -25,21 +30,7 @@ public:
     using const_reference = const T &;
     using size_type = typename vector_type::size_type;
     using difference_type = typename vector_type::difference_type;
-
-    // typedef typename vector_type::iterator          iterator;
-    // typedef typename vector_type::const_iterator    const_iterator;
-    // typedef typename vector_type::reverse_iterator  reverse_iterator;
-    // typedef typename vector_type::const_reverse_iterator
-                                                    const_reverse_iterator;
-
-    inline iterator begin ()  { return (vector_.begin ()); }
-    inline iterator end ()  { return (vector_.end ()); }
-    inline const_iterator begin() const { return (vector_.begin ()); }
-    inline const_iterator end () const { return (vector_.end ()); }
-    inline reverse_iterator rbegin ()  { return (vector_.rbegin ()); }
-    inline reverse_iterator rend () throw ()  { return (vector_.rend ()); }
-    inline const_reverse_iterator rbegin() const { return (vector_.rbegin()); }
-    inline const_reverse_iterator rend() const { return (vector_.rend ()); }
+    using underlying_vector_type = std::vector<T>;
 
     inline size_type size() const { return (vector_.size ()); }
     inline size_type max_size() const { return (vector_.max_size()); }
@@ -47,62 +38,48 @@ public:
 
     inline bool empty() const { return (vector_.empty()); }
 
-    inline reference operator[](size_type n)  { return (*vector_[n]); }
+    inline reference operator[](size_type n)  { return (*(vector_[n])); }
     inline const_reference
-	operator[](size_type n) const { return (*vector_[n]); }
+	operator[](size_type n) const { return (*(vector_[n])); }
 
     inline void reserve (size_type n) { vector_.reserve (n); }
 
-    inline reference front ()  { return (*vector_.front ()); }
-    inline const_reference front () const { return (*vector_.front ()); }
-    inline reference back () { return (*vector_.back ()); }
-    inline const_reference back () const { return (*vector_.back ()); }
+    inline reference front ()  { return (*(vector_.front ())); }
+    inline const_reference front () const { return (*(vector_.front ())); }
+    inline reference back () { return (*(vector_.back ())); }
+    inline const_reference back () const { return (*(vector_.back ())); }
+    inline void swap (VectorPtrView &rhs)  { vector_.swap (rhs); }
 
-    inline friend bool operator == (const VectorPtrView &lhs,
-                                    const VectorPtrView &rhs) {
+    inline friend bool
+    operator == (const VectorPtrView &lhs, const VectorPtrView &rhs) {
 
         return (lhs.vector_ == rhs.vector_);
     }
-    inline friend bool operator < (const VectorPtrView &lhs,
-                                   const VectorPtrView &rhs) {
+    inline friend bool
+    operator < (const VectorPtrView &lhs, const VectorPtrView &rhs) {
 
         return (lhs.vector_ < rhs.vector_);
     }
 
     inline VectorPtrView () = default;
     inline VectorPtrView (const VectorPtrView &) = default;
+    inline VectorPtrView (VectorPtrView &&) = default;
     ~VectorPtrView () = default;
 
     inline VectorPtrView &operator = (const VectorPtrView &) = default;
+    inline VectorPtrView &operator = (VectorPtrView &&) = default;
 
-    iterator erase (iterator pos) throw ();
-    iterator erase (iterator fisrt, iterator last) throw ();
-    inline void clear () throw ()  { erase (begin (), end ()); }
+    iterator erase (iterator pos)  { return (vector_.erase (pos)); }
+    iterator erase (iterator fisrt, iterator last)  {
 
-    inline void resize (size_type n) throw ()  {
+        return (vector_.erase (first, last));
+	}
+    inline void clear () throw ()  { vector_.clear(); }
 
-        const   size_type   current_size = vector_.size ();
-
-        if (n < current_size)
-            for (size_type idx = n; idx < current_size; ++idx)
-                delete vector_ [idx];
-
-        vector_.resize (n, NULL);
-    }
-
-   // The followings are some convenient methods that are not a part
-   // of STL.
-   //
-    bool erase (const value_type &x) throw ();
-    iterator find (const data_type &x) throw ();
-    const_iterator find (const data_type &x) const throw ();
+    inline void resize (size_type n) throw ()  { vector_.resize(n); }
 };
 
-// ----------------------------------------------------------------------------
-
-#ifdef DMS_INCLUDE_SOURCE
-#  include <VectorPtrView.tcc>
-#endif // DMS_INCLUDE_SOURCE
+} // namespace hmdf
 
 // ----------------------------------------------------------------------------
 
---
+++ src/vector_ptr_view_tester.cc
@@ -0,0 +1,27 @@
+// Hossein Moein
+// April 29, 2019
+// Copyright (C) 2019-2021 Hossein Moein
+// Distributed under the BSD Software License (see file License)
+
+#include <cstdlib>
+#include <iostream>
+
+#include <VectorPtrView.h>
+
+using namespace std;
+using namespace hmdf;
+
+//-----------------------------------------------------------------------------
+
+int main (int argCnt, char *argVctr [])  {
+
+    return (EXIT_SUCCESS);
+}
+
+//-----------------------------------------------------------------------------
+
+// Local Variables:
+// mode:C++
+// tab-width:4
+// c-basic-offset:4
+// End:

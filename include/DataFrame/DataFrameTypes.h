5d6858854beb0ab8aab55e3a2cfd93fc8a7f1a33
hossein.moein@kensho.com <hossein.moein@kensho.com>
2019-07-24 08:56:28 -0400

    added json io option

--- include/DataFrame/DataFrameTypes.h
+++ include/DataFrame/DataFrameTypes.h
@@ -102,6 +102,7 @@ enum class drop_policy : unsigned char  {
 
 enum class io_format : unsigned char  {
     csv = 1,
+    json = 2,
 };
 
 // ----------------------------------------------------------------------------

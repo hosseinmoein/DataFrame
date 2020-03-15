7b77255e5c5bd76a2bb478dbeb0c79ee5e2ca1d7
Hossein Moein <31854960+hosseinmoein@users.noreply.github.com>
2020-03-15 14:37:59 -0400

    fixed codacy issues

--- test/gen_rand_tester.cc
+++ test/gen_rand_tester.cc
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]) {
         p.min_value = 0;
         p.max_value = 1000000;
 
-        auto    r = gen_uniform_int_dist<long>(1024, p);
+        gen_uniform_int_dist<long>(1024, p);
     }
 
     {
@@ -52,19 +52,11 @@ int main(int argc, char *argv[]) {
         p.max_value = 2.0;
         p.seed = 23;
 
-        auto    r = gen_uniform_real_dist<double>(1024, p);
+        gen_uniform_real_dist<double>(1024, p);
     }
 
     {
-        auto    r = gen_bernoulli_dist(1024);
-    }
-
-    {
-        RandGenParams<int>  p;
-
-        p.t_dist = 1000;
-
-        auto    r = gen_binomial_dist<int>(1024, p);
+        gen_bernoulli_dist(1024);
     }
 
     {
@@ -72,11 +64,19 @@ int main(int argc, char *argv[]) {
 
         p.t_dist = 1000;
 
-        auto    r = gen_negative_binomial_dist<int>(1024, p);
+        gen_binomial_dist<int>(1024, p);
     }
 
     {
-        auto    r = gen_geometric_dist<int>(1024);
+        RandGenParams<int>  p;
+
+        p.t_dist = 1000;
+
+        gen_negative_binomial_dist<int>(1024, p);
+    }
+
+    {
+        gen_geometric_dist<int>(1024);
     }
 
     {
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
 
         p.mean = 4.0;
 
-        auto    r = gen_poisson_dist<int>(1024, p);
+        gen_poisson_dist<int>(1024, p);
     }
 
     {
@@ -92,7 +92,7 @@ int main(int argc, char *argv[]) {
 
         p.lambda = 1.5;
 
-        auto    r = gen_exponential_dist<double>(1024, p);
+        gen_exponential_dist<double>(1024, p);
     }
 
     {
@@ -101,7 +101,7 @@ int main(int argc, char *argv[]) {
         p.alpha = 1.5;
         p.beta = 1.5;
 
-        auto    r = gen_gamma_dist<double>(1024, p);
+        gen_gamma_dist<double>(1024, p);
     }
 
     {
@@ -110,7 +110,7 @@ int main(int argc, char *argv[]) {
         p.alpha = 1.5;
         p.beta = 1.5;
 
-        auto    r = gen_weibull_dist<double>(1024, p);
+        gen_weibull_dist<double>(1024, p);
     }
 
     {
@@ -119,7 +119,7 @@ int main(int argc, char *argv[]) {
         p.alpha = 1.5;
         p.beta = 1.5;
 
-        auto    r = gen_extreme_value_dist<double>(1024, p);
+        gen_extreme_value_dist<double>(1024, p);
     }
 
     {
@@ -136,7 +136,7 @@ int main(int argc, char *argv[]) {
     }
 
     {
-        auto    r = gen_lognormal_dist<double>(1024);
+        gen_lognormal_dist<double>(1024);
     }
 
     {
@@ -144,15 +144,15 @@ int main(int argc, char *argv[]) {
 
         p.n = 4.0;
 
-        auto    r = gen_chi_squared_dist<double>(1024, p);
+        gen_chi_squared_dist<double>(1024, p);
     }
 
     {
-        auto    r = gen_cauchy_dist<double>(1024);
+        gen_cauchy_dist<double>(1024);
     }
 
     {
-        auto    r = gen_fisher_f_dist<double>(1024);
+        gen_fisher_f_dist<double>(1024);
     }
 
     {

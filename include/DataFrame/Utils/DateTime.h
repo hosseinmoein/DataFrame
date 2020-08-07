ec4972166b1565bc610d3a726f9e8dd9faff8f8e
hossein.moein@kensho.com <hossein.moein@kensho.com>
2020-08-07 10:25:25 -0400

    Fixed the DateTime Windows compilation

--- include/DataFrame/Utils/DateTime.h
+++ include/DataFrame/Utils/DateTime.h
@@ -36,12 +36,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <string>
 #include <sys/timeb.h>
 
-#if defined(_WIN32) && defined(HMDF_SHARED)
-#  ifdef LIBRARY_EXPORTS
-#    define LIBRARY_API __declspec(dllexport)
-#  else
-#    define LIBRARY_API __declspec(dllimport)
-#  endif // LIBRARY_EXPORTS
+#if defined(_WIN32)
+#  include <windows.h>
+#  if defined(HMDF_SHARED)
+#    ifdef LIBRARY_EXPORTS
+#      define LIBRARY_API __declspec(dllexport)
+#    else
+#      define LIBRARY_API __declspec(dllimport)
+#    endif // LIBRARY_EXPORTS
+#  endif // HMDF_SHARED
 #  ifdef min
 #    undef min
 #  endif // min

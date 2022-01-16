365eeb55f1bd21e76d986888ecd865af2eb2b316
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2022-01-16 01:26:12 +0100

    refactoring of CMake files
    - bump VERSION in CMake to current DataFrame version
    - no RPATH manipulation anymore
    - no uninstall target, it is not a recommended practice in CMake
    - no export compile commands by default
    - don't suggest CMAKE_BUILD_TYPE
    - Don't rely on PROJECT_NAME, it's a bad practice usually
    - add DataFrame::DataFrame ALIAS target
    - remove listing of header files, it's useless
    - define SOVERSION
    - inject required system libs in installed pkgconfig file
    - rely on built-in CMake module files to create CMake config file
    - move tests logic in test subdir
    - test dataframe_tester_3, it was built but not added to ctest
    - if missing or modified, copy each relevant resource file to build folder at build time

---
+++ cmake/dataframe_target_data_files.cmake
@@ -0,0 +1,23 @@
+function(dataframe_target_data_files)
+    cmake_parse_arguments(ARG "" "TARGET" "DATA_FILES" ${ARGN})
+
+    foreach(_data_file IN LISTS ARG_DATA_FILES)
+        get_filename_component(_data_file_name ${_data_file} NAME)
+        set(_data_file_target copy_${_data_file_name})
+        if(NOT TARGET ${_data_file_target})
+            set(_data_file_output
+                ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${CMAKE_CFG_INTDIR}/data/${_data_file_name})
+            add_custom_target(${_data_file_target} ALL
+                DEPENDS ${_data_file_output}
+            )
+            get_filename_component(_data_file_abs_path ${_data_file} REALPATH)
+            add_custom_command(
+                OUTPUT ${_data_file_output}
+                COMMAND ${CMAKE_COMMAND} -E copy_if_different
+                        ${_data_file_abs_path} ${_data_file_output}
+                DEPENDS ${_data_file_abs_path}
+            )
+        endif()
+        add_dependencies(${ARG_TARGET} ${_data_file_target})
+    endforeach()
+endfunction()

96481eecd48896931c95cc5df9207927bbd2c190
Hossein Moein <hossein.moein@gradientboostedinvestments.com>
2022-11-06 11:13:59 -0500

    Rearranged the tests for AppVeyor to succeed 2

--- test/dataframe_tester_2.cc
+++ test/dataframe_tester_2.cc
@@ -4198,6 +4198,145 @@ static void test_get_data()  {
 
 // -----------------------------------------------------------------------------
 
+static void test_get_data_by_sel()  {
+
+    std::cout << "\nTesting get_data_by_sel() ..." << std::endl;
+
+    std::vector<unsigned long>  idx =
+        { 123450, 123451, 123452, 123453, 123454, 123455, 123456 };
+    std::vector<double> d1 = { 1, 2, 3, 4, 5, 6, 7 };
+    std::vector<double> d2 = { 8, 9, 10, 11, 12, 13, 14 };
+    std::vector<double> d3 = { 15, 16, 17, 18, 19, 20, 21 };
+    std::vector<double> d4 = { 22, 23, 24, 25 };
+    std::vector<std::string> s1 = { "11", "22", "33", "ee", "ff", "gg", "ll" };
+    MyDataFrame         df;
+
+    df.load_data(std::move(idx),
+                 std::make_pair("col_1", d1),
+                 std::make_pair("col_2", d2),
+                 std::make_pair("col_3", d3),
+                 std::make_pair("col_str", s1));
+    df.load_column("col_4", std::move(d4), nan_policy::dont_pad_with_nans);
+
+    auto    functor =
+        [](const unsigned long &, const double &val)-> bool {
+            return (val >= 5);
+        };
+    auto    result =
+        df.get_data_by_sel<double, decltype(functor), double, std::string>
+            ("col_1", functor);
+
+    assert(result.get_index().size() == 3);
+    assert(result.get_column<double>("col_1").size() == 3);
+    assert(result.get_column<std::string>("col_str").size() == 3);
+    assert(result.get_column<double>("col_4").size() == 0);
+    assert(result.get_index()[0] == 123454);
+    assert(result.get_index()[2] == 123456);
+    assert(result.get_column<double>("col_2")[1] == 13);
+    assert(result.get_column<std::string>("col_str")[1] == "gg");
+    assert(result.get_column<std::string>("col_str")[2] == "ll");
+    assert(result.get_column<double>("col_1")[1] == 6);
+    assert(result.get_column<double>("col_1")[2] == 7);
+
+    auto    functor2 =
+        [](const unsigned long &,
+           const double &val1,
+           const double &val2,
+           const std::string &val3)-> bool {
+            return (val1 >= 5 || val2 == 15 || val3 == "33");
+        };
+    auto    result2 =
+        df.get_data_by_sel<double,
+                           double,
+                           std::string,
+                           decltype(functor2),
+                           double, std::string>
+        ("col_1", "col_3", "col_str", functor2);
+
+    assert(result2.get_index().size() == 5);
+    assert(result2.get_column<double>("col_1").size() == 5);
+    assert(result2.get_column<std::string>("col_str").size() == 5);
+    assert(result2.get_column<double>("col_4").size() == 2);
+    assert(result2.get_index()[0] == 123450);
+    assert(result2.get_index()[2] == 123454);
+    assert(result2.get_index()[4] == 123456);
+    assert(result2.get_column<double>("col_2")[0] == 8);
+    assert(result2.get_column<double>("col_2")[1] == 10);
+    assert(result2.get_column<double>("col_2")[3] == 13);
+    assert(result2.get_column<double>("col_4")[0] == 22);
+    assert(result2.get_column<double>("col_4")[1] == 24);
+    assert(result2.get_column<std::string>("col_str")[0] == "11");
+    assert(result2.get_column<std::string>("col_str")[1] == "33");
+    assert(result2.get_column<std::string>("col_str")[2] == "ff");
+    assert(result2.get_column<std::string>("col_str")[4] == "ll");
+    assert(result2.get_column<double>("col_1")[0] == 1);
+    assert(result2.get_column<double>("col_1")[1] == 3);
+    assert(result2.get_column<double>("col_1")[2] == 5);
+
+    std::vector<std::string> s2 = { "aa", "bb", "cc", "10", "11", "12", "14" };
+
+    df.load_column("col_str2", s2);
+
+    auto    functor3 =
+        [](const unsigned long &,
+           const double &val1,
+           const double &,
+           const double &val3,
+           const std::string &,
+           const std::string &val5)-> bool {
+            return (val1 >= 5 || val3 == 15 || val5 == "cc");
+        };
+    auto    result3 =
+        df.get_data_by_sel<double,
+                           double,
+                           double,
+                           std::string,
+                           std::string,
+                           decltype(functor3),
+                           double, std::string>
+        ("col_1", "col_2", "col_3", "col_str", "col_str2", functor3);
+
+    const MyDataFrame   &const_df = df;
+
+    auto    result4 =
+        df.get_view_by_sel<double,
+                           double,
+                           double,
+                           std::string,
+                           std::string,
+                           decltype(functor3),
+                           double, std::string>
+        ("col_1", "col_2", "col_3", "col_str", "col_str2", functor3);
+    auto    const_result4 =
+        const_df.get_view_by_sel<double,
+                                 double,
+                                 double,
+                                 std::string,
+                                 std::string,
+                                 decltype(functor3),
+                                 double, std::string>
+        ("col_1", "col_2", "col_3", "col_str", "col_str2", functor3);
+
+    assert(result3.get_index().size() == 5);
+    assert(result4.get_index().size() == 5);
+    assert(result3.get_column<double>("col_1").size() == 5);
+    assert(result4.get_column<double>("col_1").size() == 5);
+    assert(result3.get_column<double>("col_2")[3] == 13);
+    assert(result4.get_column<double>("col_2")[3] == 13);
+    assert(result3.get_column<std::string>("col_str")[2] == "ff");
+    assert(result4.get_column<std::string>("col_str")[2] == "ff");
+    assert(result3.get_column<double>("col_1")[2] == 5);
+    assert(result4.get_column<double>("col_1")[2] == 5);
+
+    assert(const_result4.get_index().size() == 5);
+    assert(const_result4.get_column<double>("col_1").size() == 5);
+    assert(const_result4.get_column<double>("col_2")[3] == 13);
+    assert(const_result4.get_column<std::string>("col_str")[2] == "ff");
+    assert(const_result4.get_column<double>("col_1")[2] == 5);
+}
+
+// -----------------------------------------------------------------------------
+
 int main(int, char *[]) {
 
     test_get_reindexed();
@@ -4281,6 +4420,7 @@ int main(int, char *[]) {
     test_get_view_by_loc();
     test_get_view_by_idx_slicing();
     test_get_data();
+    test_get_data_by_sel();
 
     return (0);
 }

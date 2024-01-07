637a98e740ca34e35afd8318a029c9824860b4bb
Hossein Moein <31854960+hosseinmoein@users.noreply.github.com>
2024-01-07 15:09:38 -0500

    Update hello_world.cc

--- examples/hello_world.cc
+++ examples/hello_world.cc
@@ -75,7 +75,7 @@ int main(int, char *[])  {
     // NOTE: make sure you read and understand the Multithreading section
     //       in the documentations (threads could potentially hinder performance).
     //       This program (hello world) is a perfect example. Since I know this program doesn’t
-    //       deal with large datasets to trigger multithreaded algorithms, populated the thread-pool
+    //       deal with large datasets to trigger multithreaded algorithms, populating the thread-pool
     //       with threads (i.e. calling set_optimum_thread_level()) is a waste of resources.
     //
     ThreadGranularity::set_optimum_thread_level();
